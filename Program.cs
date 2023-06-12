1f8b53403e95ad416767bb920255eee520c0a78d
Volodymyr.Kundel <Volodymyr.Kundel@VKUNDEL-NN>
2023-06-12 18:00:10 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace C__gr3_part1_HW5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("V");
+            Course course = new Course() { TeacherName = "Kris"};
+
+            student.Print();
+            Console.WriteLine();
+            course.Print();
+
+            course.NumberOfStudents++;
+
+            Console.WriteLine();
+            course.Print();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,34 @@
+﻿namespace C__gr3_part1_HW5
+{
+    public class Student
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public string City;
+
+        public Student(string firstName, string lastName, int age, string city)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            City = city;
+        }
+
+        public Student (string firstName, string lastName, int age) : this(firstName, lastName, age, "NoCity") { }
+
+        public Student (string firstName, string lastName) : this(firstName, lastName, -1) { }
+
+        public Student (string firstName) : this(firstName, "NoLastName") { }
+
+        public Student () : this("NoFirstName") { }
+
+        public void Print ()
+        {
+            Console.WriteLine("First Name is: " + FirstName);
+            Console.WriteLine("Last Name is: " + LastName);
+            Console.WriteLine("Age is: " + Age);
+            Console.WriteLine("City is: " + City);
+        }
+    }
+}
